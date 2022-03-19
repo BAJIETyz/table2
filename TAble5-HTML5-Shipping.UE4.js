63f4f3540de35baae38a32844d04dd2c02940828
BAJIETyz <101792343+BAJIETyz@users.noreply.github.com>
2022-03-19 16:57:46 +0300

    Update TAble5-HTML5-Shipping.UE4.js

--- TAble5-HTML5-Shipping.UE4.js
+++ TAble5-HTML5-Shipping.UE4.js
@@ -1155,7 +1155,7 @@ $(document).ready(function() {
 		// ----------------------------------------
 		// MORE JS
 		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/BAJIETyz/table2/2695db1bf0fa4dc24dfa3e9b774b374a959ed79c/TAble5-HTML5-Shipping.data.js'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/BAJIETyz/table2/9b7f3f98325224c7e0eedfddc96b102a87c4a2b5/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('TAble5-HTML5-Shipping.data')).then(function(dataBlob) {
