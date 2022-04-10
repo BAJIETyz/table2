72b5ee97a08f2af1388256b8a0e552ebf733fffb
BAJIETyz <101792343+BAJIETyz@users.noreply.github.com>
2022-04-10 13:27:49 +0300

    Update TAble5.data.js

--- TAble5.data.js
+++ TAble5.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'G:/Konstraktor/Migrate/TAble5/Binaries/HTML5/TAble5.data';
-    var REMOTE_PACKAGE_BASE = 'TAble5.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/kmrp25j4d05jia2/TAble5.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/kmrp25j4d05jia2/TAble5.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
